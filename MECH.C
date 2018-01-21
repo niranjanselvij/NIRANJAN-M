e3e3b072bdf6d89679b966f38a765e7174c8c8c6
niranjanselvij <35652873+niranjanselvij@users.noreply.github.com>
2018-01-21 16:02:06 +0530

    Create MECH.C

---
+++ MECH.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+voidmain()
+{int a,b,N;
+
+printf("enter  value of a");
+scanf("%d",&a);
+
+if(a>0)
+{
+printf("the given number is  postive");
+}
+else
+if(a==o)
+{
+printf("the given  number is zero");
+}
+else
+{
+printf("the given number is negative");
+}
+
+
